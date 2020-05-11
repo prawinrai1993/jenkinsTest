56758cc0e4a1fa04674c54ea5f1bceb8baff48c1
prawinrai <prawinrai@hotmail.com>
2020-05-12 00:03:52 +0530

    code updated

--- execute_MathLib.c
+++ execute_MathLib.c
@@ -7,16 +7,16 @@ int main()
 	int val = sum(10,20);
 	printf("addition_result =%d\n",val);
 
-	int val = sub(10,20);
+	val = sub(10,20);
 	printf("substraction_result =%d\n",val);
 
-	int val = mul()(10,20);
+	val = mul()(10,20);
 	printf("multiplication_result =%d\n",val);
 
-	int val = mod(10,20);
+	val = mod(10,20);
 	printf("mod_result =%d\n",val);
 
-	int val = div(10,20);
+	val = div(10,20);
 	printf("div_result =%d\n",val);
 
 	// added comment
